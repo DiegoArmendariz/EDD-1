1646fb260b02d77d8fef5e90468d164efee9aa3c
DiegoArmendariz <159494528+DiegoArmendariz@users.noreply.github.com>
2026-09-07 16:47:04 -0600

    Add files via upload

---
+++ EVA1_4_OBJETOS/build/classes/eva1_4_objetos/EVA1_4_OBJETOS.rs
@@ -0,0 +1,2 @@
+eva1_4_objetos.Prueba
+eva1_4_objetos.EVA1_4_OBJETOS
---
+++ EVA1_4_OBJETOS/src/eva1_4_objetos/EVA1_4_OBJETOS.java
@@ -0,0 +1,18 @@
+package eva1_4_objetos;
+
+/**
+ *
+ * @author diegd
+ */
+public class EVA1_4_OBJETOS {
+
+  
+    public static void main(String[] args) {
+       Prueba prueba = new Prueba();
+        System.out.println(prueba);
+    }
+    
+}
+class Prueba{
+    
+}
